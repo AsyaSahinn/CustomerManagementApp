26db7a817e5857bfa5db12cf03b40f745d127350
asyasahin <asyasahin@example.com>
2023-08-14 11:59:50 +0300

    Other method annotation added

--- DAL/Concrete/AdminRepository.cs
+++ DAL/Concrete/AdminRepository.cs
@@ -6,23 +6,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CustomerManagement.DAL.Concrete
 {
+    // Admin entity'sine özgü veri tabanı işlemlerini yöneten sınıf.
     public class AdminRepository : GenericRepository<Admin>, IAdminRepository
     {
         private AppDbContext _context;
 
-        public AdminRepository(AppDbContext context) : base(context) 
+        // Constructor, AppDbContext tipinde bir nesne alır ve GenericRepository'nin constructor'ını çağırır.
+        public AdminRepository(AppDbContext context) : base(context)
         {
             _context = context;
         }
 
+        // Kullanıcı adı ve şifreye göre admin bilgilerini sorgular.
+        // Eğer admin bulunursa true, bulunmazsa false döner.
         public async Task<bool> GetAdminInfoByUsernameAndPassword(string username, string password)
         {
-            var admin= await _context.Set<Admin>().FirstOrDefaultAsync(x => x.UserName == username && x.UserPassword == password);
-            if( admin == null )
+            // Veri tabanında admin tablosunda kullanıcı adı ve şifre kontrolü yapılır.
+            var admin = await _context.Set<Admin>().FirstOrDefaultAsync(x => x.UserName == username && x.UserPassword == password);
+            if (admin == null)
             {
-                return false;
+                return false; // Eğer admin bulunamazsa false döner.
             }
-            return true;
+            return true; // Admin bulunursa true döner.
         }
     }
 }
--- DAL/Concrete/CustomerRepository.cs
+++ DAL/Concrete/CustomerRepository.cs
@@ -5,11 +5,13 @@ using CustomerManagement.Models.Entity;
 
 namespace CustomerManagement.DAL.Concrete
 {
+    // Customer entity'sine özgü veri tabanı işlemlerini yöneten sınıf.
     public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
     {
         private AppDbContext _context;
 
-        public CustomerRepository(AppDbContext context): base(context) 
+        // Constructor, AppDbContext tipinde bir nesne alır ve GenericRepository'nin constructor'ını çağırır.
+        public CustomerRepository(AppDbContext context) : base(context)
         {
             _context = context;
         }
